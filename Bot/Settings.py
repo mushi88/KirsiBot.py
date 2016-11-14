83892df588ecd06550ad8ee6cfabb4abde23cff4
Kirsi <mushi88@users.noreply.github.com>
2016-11-15 00:08:30 +1100

    Update Settings.py

--- Bot/Settings.py
+++ Bot/Settings.py
@@ -6,3 +6,4 @@ User_ID="User ID" ## Put your User ID Here. Adds you to "Full" access.
 Voice_Channel="Voice Channel ID" ## Replace Voice Channel ID with the id of the voice channel you want the bot to autojoin.
 Server="Server ID" ## Replace Server ID with the id of the server that holds the channel your bot will autojoin.
 Chat_Channel="Chat Channel ID" ## Replace Chat Channel ID with the text channel that the music bot will only accept commands from.
+Prefix="~" ## Default Prefix, resets to this on bot restart.
