7788de70f9a78ad16035ce4ff2b183f9764dc657
Kirsi <mushi88@users.noreply.github.com>
2017-01-24 01:09:31 +1100

    Fix #1

--- Kirsi Bot.py
+++ Kirsi Bot.py
@@ -21,8 +21,6 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-sys.setrecursionlimit(sys.maxsize) # TODO: Fix crashing. [Cause may be thread]
-
 prefix=Settings.Prefix
 client = discord.Client()
 musico=False
